8cb1ded6525cc36b561ad55a29b8fccb82aeb7fb
Mohammad Azam <azamsharp@Mohammads-MBP.localdomain>
2017-12-12 12:44:40 -0600

    removed default routes

--- Sources/App/Routes/Routes.swift
+++ Sources/App/Routes/Routes.swift
@@ -66,25 +66,7 @@ extension Droplet {
             return try FlowerService.flowers.makeJSON()
             
         }
-        
-        get("hello") { req in
-            var json = JSON()
-            try json.set("hello", "world")
-            return json
-        }
-
-        get("plaintext") { req in
-            return "Hello, world!"
-        }
-
-        // response to requests to /info domain
-        // with a description of the request
-        get("info") { req in
-            return req.description
-        }
-
-        get("description") { req in return req.description }
-        
+     
         try resource("posts", PostController.self)
     }
 }
